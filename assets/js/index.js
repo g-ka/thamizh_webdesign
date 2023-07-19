daadbfd375eac5082b62ef6d977a33c3f6039919
g-ka <110980190+g-ka@users.noreply.github.com>
2023-07-20 00:03:13 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,6 +18,23 @@ window.addEventListener('wheel', (event) =>
     }
   })
 
+window.addEventListener('touchmove', (event) =>
+  {
+    if(window.pageYOffset >= 73)
+    {
+      if (event.deltaY >= 0) 
+      {
+        //console.log('Scroll down');
+        document.querySelector('header').classList.add('header_adder');
+      }
+      else 
+      {
+        //console.log('Scroll up');
+        document.querySelector('header').classList.remove('header_adder');
+      }
+    }
+  })
+
 // hide nav_bar on dynamic scroll (and) color change of menu option
 
   let menu_links = document.querySelectorAll('.hide_nav');
@@ -78,6 +95,55 @@ window.addEventListener('wheel', (event) =>
 // automatic color change in menu_bar
 
 document.querySelector('html').addEventListener('wheel', () =>
+{
+  let about_section = document.querySelector('.about_section');
+  let roadmap_section = document.querySelector('.roadmap_section');
+  let courses_section = document.querySelector('.courses_section');
+  let contact_section = document.querySelector('.contact_section');
+
+  let home_btn = document.querySelector('.home_btn');
+  let about_btn = document.querySelector('.about_btn');
+  let roadmap_btn = document.querySelector('.roadmap_btn');
+  let courses_btn = document.querySelector('.courses_btn');
+  let contact_btn = document.querySelector('.contact_btn');
+
+  function remove_color()
+  {
+    menu_links.forEach(link =>
+      {
+        link.classList.remove('on_action');
+      })
+    document.querySelector('.home_btn').classList.remove('on_action');
+  }
+
+  if(window.pageYOffset >= 0 && window.pageYOffset < 0.8*about_section.offsetTop)
+  {
+    remove_color();
+    home_btn.classList.add('on_action');
+  } 
+  else if(window.pageYOffset >= 0.8*about_section.offsetTop && window.pageYOffset < 0.8*roadmap_section.offsetTop)
+  {
+    remove_color();
+    about_btn.classList.add('on_action');
+  }
+  else if(window.pageYOffset >= 0.8*roadmap_section.offsetTop && window.pageYOffset < 0.8*courses_section.offsetTop)
+  {
+    remove_color();
+    roadmap_btn.classList.add('on_action');
+  }
+  else if(window.pageYOffset >= 0.8*courses_section.offsetTop && window.pageYOffset < 0.8*contact_section.offsetTop)
+  {
+    remove_color();
+    courses_btn.classList.add('on_action');
+  }
+  else if(window.pageYOffset >= 0.8*contact_section.offsetTop)
+  {
+    remove_color();
+    contact_btn.classList.add('on_action');
+  }
+});
+
+document.querySelector('html').addEventListener('touchmove', () =>
 {
   let about_section = document.querySelector('.about_section');
   let roadmap_section = document.querySelector('.roadmap_section');
